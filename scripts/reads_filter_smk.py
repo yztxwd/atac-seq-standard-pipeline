3fcfb5d09aa3c47336e434bb031d35f0461e3349
Jianyu Yang <33720589+yztxwd@users.noreply.github.com>
2021-01-28 14:51:20 +0800

    Update reads_filter_smk.py
    Remove coverage and midpoint bedGraph output, bedtools bamtobed is better for this usage

--- scripts/reads_filter_smk.py
+++ scripts/reads_filter_smk.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 import sys, pysam, re
-import pandas as pd
 import numpy as np
 
 from optparse import OptionParser, IndentedHelpFormatter
@@ -21,7 +20,7 @@ Example:
 		output:
 			"mapped/{sample}-{unit, [^.]+}.F1804.filtered.bam"
 		params:
-			"-m pair -f 0 -s 0 -t 20"
+			"-m pair -f 0 -t 20"
 		conda:
 			"envs/py3.yaml"
 		script:
@@ -29,7 +28,7 @@ Example:
 
 Dependencies:
     numpy
-    pandas
+    pysam
 
 @Copyright 2020, Jianyu Yang, Southern Medical University
 """
@@ -37,7 +36,6 @@ Dependencies:
 parser = OptionParser(description=description, formatter=CustomHelpFormatter())
 parser.add_option('-m', '--mode', dest='mode', help="single/pair end mode")
 parser.add_option('-f', '--fragment-size', dest='fragment', help="fragment size threshold for paired-end reads, keep reads < threshold")
-parser.add_option('-s', '--shit-size', dest='shift', help="'shift single end reads along 5'-3'")
 parser.add_option('-t', '--mapq-threshold', dest='threshold', help="MAPQ threshold to filter reads")
 pattern = re.compile("[ ]+")
 option, argument = parser.parse_args(pattern.split(str(snakemake.params)))
@@ -47,19 +45,16 @@ input = snakemake.input[0]
 output = snakemake.output[0]
 mode = option.mode
 frag_size_threshold = int(option.fragment)
-shift_size = int(option.shift)
 mapq_threshold = int(option.threshold)
-
-prefix = output.split(".")[0]	# extract prefix for additional output
 
 #One could look at the file name too see if we're dealing with a SAM or BAM file, but this is simpler
 ifile = pysam.AlignmentFile(input, "rb")
 ofile = pysam.AlignmentFile(output, "wb", template=ifile)
-chr = []
-mid_coor = []
-left_coor = []
-right_coor = []
-name = []
+
+#Record how many reads filtered during iteration
+filter_mapq = 0
+filter_size = 0
+filter_chrom = 0
 
 #iterate
 if mode == 'pair':
@@ -76,55 +71,42 @@ if mode == 'pair':
 			if (r1.query_name != r2.query_name):
 				raise Exception("Two consecutive reads don't have the same query name! Make sure bam file has been sorted by name!")
 
-			if(r1.mapping_quality >= mapq_threshold and r2.mapping_quality >= mapq_threshold) :
-				r1_coor = r1.reference_start + 1
-				r2_coor = r2.reference_start + 1
-				if (r1.reference_name != r2.reference_name):
-					continue
-				else:
-					left = r1_coor if r1_coor < r2_coor else r2_coor
-					right = r2_coor if r1_coor < r2_coor else r1_coor
-					read_length = r2.infer_read_length() if r1_coor < r2_coor else r1.infer_read_length()
-					if (frag_size_threshold > 0):
-						if ((right - left + read_length) >= frag_size_threshold):
-							continue				
-					left_coor.append(left)
-					right_coor.append(right + read_length - 1)
-					chr.append(r1.reference_name)
-					mid_coor.append(int((r1_coor + r2_coor + read_length - 1)/2))
-					ofile.write(r1)
-					ofile.write(r2)
-					name.append("%s/%s" %(r1.qname, r2.qname))
+			if (r1.mapping_quality < mapq_threshold or r2.mapping_quality < mapq_threshold):
+				filter_mapq += 1
+				continue
+			
+			if (r1.reference_name != r2.reference_name):
+				filter_chrom += 1
+				continue
+
+			r1_coor = r1.reference_start + 1
+			r2_coor = r2.reference_start + 1
+			left = r1_coor if r1_coor < r2_coor else r2_coor
+			right = r2_coor if r1_coor < r2_coor else r1_coor
+			read_length = r2.infer_read_length() if r1_coor < r2_coor else r1.infer_read_length()
+			if (frag_size_threshold > 0) and ((right - left + read_length) >= frag_size_threshold):
+				filter_size += 1
+				continue	
+
+			ofile.write(r1)
+			ofile.write(r2)
+			name.append("%s/%s" %(r1.qname, r2.qname))
 
 elif mode == 'single':
 	for read in ifile:
-		if read.mapping_quality >= frag_size_threshold:
-			chr.append(read.reference_name)
-			ofile.write(read)
-			coor = read.reference_start + 1
-			if read.is_reverse:
-				mid_coor.append(int(coor + read.infer_read_length()/2 - shift_size-1))
-				left_coor.append(coor - shift_size)
-				right_coor.append(coor - shift_size + read.infer_read_length()-1)
-			else:
-				mid_coor.append(int(coor + read.infer_read_length()/2 + shift_size-1))
-				left_coor.append(coor + shift_size)
-				right_coor.append(coor + shift_size + read.infer_read_length()-1)
-			name.append(read.qname)
+		if read.mapping_quality < mapq_threshold:
+			filter_mapq += 1
+		
+		ofile.write(read)
 else:
 	raise Exception("Please input a valid mode: single/pair")
 		
 ifile.close()
 ofile.close()
-coverage = pd.DataFrame({'chr':chr, 'left':left_coor, 'right':right_coor})
-coverage['depth'] = 1
-coverage['name'] = name
-midpoint = pd.DataFrame({'chr':chr, 'mid_coor': mid_coor})
-midpoint['right'] = midpoint['mid_coor']
-midpoint['depth'] = 1
-midpoint['name'] = name
-coverage.to_csv(prefix + '.coverage.1b.bg', header=None, sep='\t', index=False)
-midpoint.to_csv(prefix + '.midpoint.1b.bg', header=None, sep='\t', index=False)
-
+print(f"""
+Reads filtered due to MAPQ < {mapq_threshold}: {filter_mapq * 2 if mode=="pair" else filter_mapq}
+Reads filtered due to fragment size >= {frag_size_threshold}: {filter_size * 2}
+Reads filtered due to inconsistent chromosome name: {filter_chrom}
+""")
 
 
